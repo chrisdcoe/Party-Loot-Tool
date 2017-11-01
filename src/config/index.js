13906f8b8e8c7ffdaac70c9b0adab01c474efb78
Christopher Coe <christopherdcoe@gmail.com>
2017-11-01 02:26:27 -0400

    Updated paths to connect with online mongodb hosting

--- src/config/index.js
+++ src/config/index.js
@@ -2,9 +2,18 @@
 
 module.exports = {
   appName: 'Party Loot Tool',
-  port: 3030,
+
+  // *** WHEN LOCALLY HOSTING MongoDB ***
+  // port: 3030,
+  // db: {
+  //   host: 'localhost',
+  //   dbName: 'partyLoot'
+  // }
+
+  // *** HOSTED BY mLab ***
+  port: 80,
   db: {
-    host: 'localhost',
-    dbName: 'partyLoot'
+    host: 'cdcoe:c2^)9r4^14kImRM@ds023644.mlab.com:23644',
+    dbName: 'partyloot'
   }
 };
